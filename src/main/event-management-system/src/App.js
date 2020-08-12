a10759e83aa27b6e2bc29bd5e196cc5de70a7213
Haticenuragba <hnagba@gmail.com>
2020-08-12 13:15:51 +0300

    search is done

--- src/main/event-management-system/src/App.js
+++ src/main/event-management-system/src/App.js
@@ -1,62 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import CustomAppBar from "./common/CustomAppBar";
 import {getIsDark, setIsDark} from "./common/Utils";
-import AddEventForm from "./usecases/admin/add-event/AddEventForm";
-import {BrowserRouter, BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import EventDetail from "./usecases/end-user/event-application/EventDetail";
 import EventsGrid from "./usecases/end-user/list-all-events/EventsGrid";
-import {useHistory, withRouter} from "react-router";
-import {createBrowserHistory} from 'history';
-import {render} from "react-dom";
-
-export const customHistory = createBrowserHistory();
-
-
-function App(factory, deps) {
-
-    const history = useHistory();
-    const theme = React.useMemo(
-        () =>
-            createMuiTheme({
-                palette: {
-                    type: getIsDark() ? "dark" : "light",
-                },
-            }),
-    );
-
-    let [state, setState] = React.useState({textToSearch: ''});
+import {withRouter} from "react-router-dom";
+import {Route} from "react-router-dom";
+import EventDetail from "./usecases/end-user/event-application/EventDetail";
+import AddEventForm from "./usecases/admin/add-event/AddEventForm";
 
 
-    const handleModeChange = (isDark) => {
+const themeDark = createMuiTheme({
+    palette: {
+        type: "dark"
+    },
+});
+
+const themeLight = createMuiTheme({
+    palette: {
+        type: "light"
+    },
+});
+class App extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            textToSearch: ''
+        }
+    }
+
+    handleModeChange = (isDark) => {
         setIsDark(isDark);
-        let newState = state;
-        setState(newState);
+        let newState = this.state;
+        this.setState(newState);
     }
 
-    const handleSearchChanged = (text) => {
-        let newState = state;
+    handleSearchChanged = (text) => {
+        let newState = this.state;
         newState.textToSearch = text;
-        setState(newState);
+        this.setState(newState);
     }
 
-    const handleNavigateToHomePage = () => {
-        history.push('/');
+    handleNavigateToHomePage = () => {
+        this.props.history.push('/');
     }
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <CustomAppBar onModeChange={handleModeChange}
-                          onSearchChanged={handleSearchChanged}
-                          onNavigateHomePage={handleNavigateToHomePage}/>
+    render() {
+        return (
+
+            <ThemeProvider theme={getIsDark() ? themeDark : themeLight}>
+                <CssBaseline/>
+                <CustomAppBar onModeChange={this.handleModeChange}
+                              onSearchChanged={this.handleSearchChanged}
+                              onNavigateHomePage={this.handleNavigateToHomePage}/>
                 <div>
-                    <EventsGrid textToSearch={state.textToSearch} try={"Merabaaa"}/>
+                    <Route  path="/events/:eventTitle" component={withRouter(EventDetail)} />
+                    <Route  path="/add-event" component={withRouter(AddEventForm)} />
+                    <Route exact path="/" >
+                        <EventsGrid textToSearch={this.state.textToSearch}/>
+                    </Route>
+
                 </div>
-            {/*<EventDetail data={{eventTitle: "Deneme"}}/>*/}
-        </ThemeProvider>
-    );
+                {/*<EventDetail data={{eventTitle: "Deneme"}}/>*/}
+            </ThemeProvider>
+
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
